21303ce3468a4ce7391c492b7bd7fdd9ffa2b70a
fatuumb <ndeyefatoumbengue20@gmail.com>
2023-03-02 19:45:38 +0000

    modif

--- POO/POO.py
+++ POO/POO.py
@@ -2,18 +2,18 @@
 from csv import DictReader
 
 class Etudiant:
-
+  
   nameinfichier_csv = dict()
-with open('Project.csv','r') as fichier_csv:
+  with open('Project.csv','r') as fichier_csv:
 
     fichier_csv_reader = DictReader(fichier_csv)
-for line in fichier_csv_reader:
-  def __init__(self):
-    self.prenom = line[1]
-    self.nom    = line[2]
-    self.Notes  = line[3]
-    self.Numeros= line[3]
-    self.Classe = line[5]
+    for line in fichier_csv_reader:
+      def __init__(self):
+          self.prenom = line[1]
+          self.nom    = line[2]
+          self.Notes  = line[3]
+          self.Numeros= line[3]
+          self.Classe = line[5]
 
  
       # print("\n\n")
